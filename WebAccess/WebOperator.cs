3e1b0f7713d43a640e860b0c6e1212f213538ffb
Erik Nagel <erik@reallyhuman.net>
2025-01-03 16:38:18 +0100

    Initial commit.

---
+++ WebAccess/WebOperator.cs
@@ -0,0 +1,153 @@
+﻿namespace WebAccess
+{
+    public class WebOperator : IDisposable
+    {
+        #region IDisposable Member
+
+        private bool _disposed; // = false wird vom System vorbelegt;
+
+        /// <summary>
+        /// Öffentliche Methode zum Aufräumen.
+        /// </summary>
+        public void Dispose()
+        {
+            this.Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        /// <summary>
+        /// Hier wird aufgeräumt.
+        /// </summary>
+        /// <param name="disposing">False, wenn vom eigenen Destruktor auferufen.</param>
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing && !this._disposed)
+            {
+            }
+        }
+
+        /// <summary>
+        /// Finalizer: wird vom GarbageCollector aufgerufen.
+        /// </summary>
+        ~WebOperator()
+        {
+            this.Dispose(false);
+        }
+
+        #endregion IDisposable Member
+
+        public Exception? GetAndResetTaskException()
+        {
+            Exception? ex = this._taskException;
+            this._taskException = null;
+            return ex;
+        }
+
+        public void DownloadFile(string url, string savePath, IProgress<int>? progress = null)
+        {
+            DownloadFileAsync(url, savePath, progress).Wait();
+        }
+
+        public async Task DownloadFileAsync(string url, string savePath, IProgress<int>? progress = null)
+        {
+            this._taskException = null;
+            try
+            {
+                long fileSize = await GetFileSizeAsync(url);
+                // Console.WriteLine($"Dateigröße: {fileSize} Bytes");
+
+                if (fileSize > 0)
+                {
+                    await DownloadFileAsync(url, savePath, fileSize, progress);
+                    // Console.WriteLine("Download abgeschlossen.");
+                    progress?.Report(100);
+                }
+                else
+                {
+                    throw new Exception("Die Dateigröße ist 0 Byte.");
+                    // Console.WriteLine("Die Datei ist leer oder konnte nicht gefunden werden.");
+                }
+            }
+            catch (Exception ex)
+            {
+                _taskException = ex;
+            }
+
+            //int totalSchritte = 100;
+            //for (int i = 0; i < totalSchritte; i++)
+            //{
+            //    await Task.Delay(50);
+            //    // Melde den Fortschritt
+            //    progress?.Report((i + 1) * 100 / totalSchritte);
+            //}
+            //progress?.Report(100);
+        }
+
+        private Exception? _taskException;
+
+        // Methode zum Abrufen der Dateigröße
+        static async Task<long> GetFileSizeAsync(string url)
+        {
+            using (var client = new HttpClient())
+            {
+                try
+                {
+                    // HTTP-Head-Request senden, um die Dateigröße zu ermitteln
+                    var response = await client.SendAsync(new HttpRequestMessage(HttpMethod.Head, url));
+                    response.EnsureSuccessStatusCode();
+
+                    // Hole die "Content-Length" Header-Information
+                    if (response.Content.Headers.ContentLength.HasValue)
+                    {
+                        return response.Content.Headers.ContentLength.Value;
+                    }
+                    else
+                    {
+                        throw new Exception("Die Dateigröße konnte nicht ermittelt werden.");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception("Fehler beim Abrufen der Dateigröße: " + ex.Message);
+                }
+            }
+        }
+
+        // Methode zum Herunterladen der Datei mit Fortschrittsanzeige
+        static async Task DownloadFileAsync(string url, string savePath, long totalSize, IProgress<int>? progress = null)
+        {
+            using (var client = new HttpClient())
+            {
+                try
+                {
+                    var response = await client.GetAsync(url, HttpCompletionOption.ResponseHeadersRead);
+                    response.EnsureSuccessStatusCode();
+
+                    using (var stream = await response.Content.ReadAsStreamAsync())
+                    using (var fileStream = new FileStream(savePath, FileMode.Create, FileAccess.Write, FileShare.None))
+                    {
+                        byte[] buffer = new byte[8192]; // 8 KB Puffer
+                        long totalBytesRead = 0;
+                        int bytesRead;
+
+                        // Fortschritt während des Downloads
+                        while ((bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length)) > 0)
+                        {
+                            await fileStream.WriteAsync(buffer, 0, bytesRead);
+                            totalBytesRead += bytesRead;
+
+                            // Berechne und zeige den Fortschritt an
+                            int progressPercent = (int)((double)totalBytesRead / totalSize * 100);
+                            progress?.Report(progressPercent);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception("Fehler beim Herunterladen der Datei: " + ex.Message);
+                }
+            }
+        }
+
+    }
+}
---
+++ WebAccessDemo/MainWindow.xaml.cs
@@ -0,0 +1,128 @@
+﻿using System.ComponentModel;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using WebAccess;
+
+namespace WebAccessDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public string Url
+        {
+            get
+            {
+                return this._url;
+            }
+            set
+            {
+                if (this._url != value)
+                {
+                    this._url = value;
+                    OnPropertyChanged(nameof(Url));
+                }
+            }
+        }
+
+        public string TargetDirectory
+        {
+            get
+            {
+                return _targetDirectory;
+            }
+            set
+            {
+                if (this._targetDirectory != value)
+                {
+                    this._targetDirectory = value;
+                    OnPropertyChanged(nameof(TargetDirectory));
+                }
+            }
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Url = "https://neteti.de/Vishnu.doc.de/Vishnu_doc.de.chm";
+            this.TargetDirectory = Path.Combine(Path.GetTempPath(), "Vishnu_doc.de.chm");
+            this._isDownloadPossible = true;
+        }
+
+        public bool IsDownloadPossible
+        {
+            get { return this._isDownloadPossible; }
+            set
+            {
+                this._isDownloadPossible = value;
+            }
+        }
+
+        private bool _isDownloadPossible;
+        private string _url = "";
+        private string _targetDirectory = "";
+
+        private void CmdBtnDownload_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = this.IsDownloadPossible;
+        }
+
+        private async void CmdBtnDownload_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            this.lblProgress.Content = "Operation gestartet.";
+            this.pbProgress.Value = 0;
+            this.IsDownloadPossible = false;
+            await Task.Delay(500);
+
+            using (WebOperator webOperator = new WebOperator())
+            {
+                var progress = new Progress<int>(wert =>
+                {
+                    this.pbProgress.Value = wert;
+                    {
+                        this.percentageText.Text = $"{wert}%";
+                    }
+                    if (wert >= 100)
+                    {
+                        this.lblProgress.Content = "Operation abgeschlossen.";
+                    }
+                    // CommandManager.InvalidateRequerySuggested(); // erzwingt die Neuberechnung der CanExecute-Methoden
+                });
+                await webOperator.DownloadFileAsync(this.Url, this.TargetDirectory, progress); // blockiert den UI-Thread nicht
+                await Task.Delay(1000);
+                this.lblProgress.Content = "Web-Datei-Download";
+                this.IsDownloadPossible = true;
+                CommandManager.InvalidateRequerySuggested(); // erzwingt die Neuberechnung der CanExecute-Methoden
+                Exception? ex = webOperator.GetAndResetTaskException();
+                if (ex != null)
+                {
+                    MessageBox.Show(ex.Message, "Fehler", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+        }
+
+        private void CmdBtnSearch_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true; // immer möglich, da ohnehin modal
+        }
+
+        private void CmdBtnSearch_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            var dialog = new Microsoft.Win32.OpenFolderDialog();
+            if (dialog.ShowDialog() == true)
+            {
+                TargetDirectory = dialog.FolderName;
+            }
+        }
+    }
+}
